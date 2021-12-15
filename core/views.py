22da464cd4f665ff1a55b1acd456672eb93a9300
Mr-Root1 <romanescobar1029@gmail.com>
2021-12-15 17:37:05 -0500

    final

--- core/views.py
+++ core/views.py
@@ -1,12 +1,51 @@
+from socialu.models import Image, SocialPost, SocialComment
 from django.views.generic import TemplateView, View
 from django.shortcuts import redirect, render, get_object_or_404, redirect
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from socialu.forms import SocialPostForm, ShareForm
 
 
-class HomeView(View):
+class HomeView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
-        
-        contexto={
+        logged_in_user=request.user
 
+        posts = SocialPost.objects.all()
+        form = SocialPostForm()
+        share_form=ShareForm()
+
+        
+        context={
+            'form':form,
+            'posts':posts,
+           
         }
-        return render(request, 'pages/index.html', contexto)
+        return render(request, 'pages/index.html', context)
+
+    def post(self, request, *args, **kwargs):
+        logged_in_user=request.user
+
+        posts = SocialPost.objects.all()
+        form = SocialPostForm(request.POST, request.FILES)
+        files = request.FILES.getlist('image')
+
+        share_form=ShareForm()
+
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.author = logged_in_user
+            new_post.save()
+
+            for f in files:
+                img = Image(image=f)
+                img.save()
+                new_post.image.add(img)
+
+            new_post.save()
+
+        
+        context={
+            'form':form,
+            'posts':posts,
+        }
+        return render(request, 'pages/index.html', context)
--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -89,5 +89,6 @@ module.exports = {
         require('@tailwindcss/typography'),
         require('@tailwindcss/line-clamp'),
         require('@tailwindcss/aspect-ratio'),
+        require('tailwind-scrollbar-hide'),
     ],
 }
